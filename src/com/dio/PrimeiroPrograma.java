fe1f0ab8074ececeef93bcf5ba2b5fc1725200f2
AndressaCKVS <andressa.ckvs@gmail.com>
2022-07-13 09:34:20 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		
+	}
+}
